ddf1582e61b23848d01737f555b7c651aba783cb
dojo-json <jfranz@codingdojo.com>
2019-01-16 18:07:06 -0800

    js objects

--- week1/day1/main.js
+++ week1/day1/main.js
@@ -1,29 +1,29 @@
 var index;
 var myString = 'some string content';
 
-console.log(myString);
+// console.log(myString);
 
-myString = 345;
+// myString = 345;
 
-console.log(myString);
-//  offset    0, 1, 2 
-const array = ['cat', 'dog', 'horse'];
+// console.log(myString);
+// //  offset    0, 1, 2
+// const array = ['cat', 'dog', 'horse'];
 
-console.log(array.push(myString));
+// console.log(array.push(myString));
 
-console.log(array[1]);
+// console.log(array[1]);
 
-console.log(array.pop(1))
+// console.log(array.pop(1))
 
-console.log(array);
+// console.log(array);
 
-console.log('index before loop', index);
+// console.log('index before loop', index);
 
-for (let index = 0; index < array.length; index++) {
-  console.log('index', index, array[index]);
-}
+// for (let index = 0; index < array.length; index++) {
+//   console.log('index', index, array[index]);
+// }
 
-console.log('index after loop', index);
+// console.log('index after loop', index);
 
 // for (var element in array) {
 //   console.log('element in for-in', array[element]);
@@ -48,7 +48,7 @@ person['weight'] = 183;
 
 
 for (var key in person) {
-  console.log('key', key, person[key]);
+  // console.log('key', key, person[key]);
 }
 
 function sayHello(name, ...rest) {
@@ -61,4 +61,32 @@ function sayHello(name, ...rest) {
   }
 }
 
-sayHello('Jason', true, 234234);
+// sayHello('Jason', true, 234234);
+
+
+function counter() {
+  var count = 0;
+
+  function childScope() {
+    count += 1;
+    return count;
+  }
+
+  return childScope;
+}
+
+counter = counter();
+
+console.log(counter());
+// => 1
+
+console.log(counter());
+// => 2
+console.log(counter());
+// => 3
+console.log(counter());
+// => 4
+console.log(counter());
+// => 5
+console.log(counter());
+// => 6
